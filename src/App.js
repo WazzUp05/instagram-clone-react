a784f7d0041d895ed3211999f6f9c299db22f173
WazzUp05 <49414448+WazzUp05@users.noreply.github.com>
2020-08-21 01:06:21 +0600

    create 1:01:27

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Post from "./Post";
 
 function App() {
+  const [posts, setPost] = useState([
+    {
+      username: "vasiliyribakov",
+      caption: "Все будет заебись",
+      imageUrl:
+        "https://instagram.fkgf1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/101035703_688480591928696_4996110215808425238_n.jpg?_nc_ht=instagram.fkgf1-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=IxKmzmUBoLgAX8WBgA0&oh=1c5e28232832e2413fad61b00f917472&oe=5F6A089B",
+    },
+    {
+      username: "vasiliyribakov",
+      caption: "Все будет заебись",
+      imageUrl:
+        "https://instagram.fkgf1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/105942212_265299748030494_7274953083073322176_n.jpg?_nc_ht=instagram.fkgf1-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=vccs_C0eGz0AX-KIerd&oh=3d7e6d8965787b7444daeb9f7aefe506&oe=5F69EF9E",
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app_headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+          alt=""
+        />
+      </div>
+
+      {posts.map((post) => (
+        <Post
+          username={post.username}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDx5A8YiU6fnNze0-Z1UtM1bk_YbQJpoHs",
+  authDomain: "instagram-clone-react-9a89b.firebaseapp.com",
+  databaseURL: "https://instagram-clone-react-9a89b.firebaseio.com",
+  projectId: "instagram-clone-react-9a89b",
+  storageBucket: "instagram-clone-react-9a89b.appspot.com",
+  messagingSenderId: "543251687226",
+  appId: "1:543251687226:web:256ec5a4ef925b88109203",
+  measurementId: "G-MDFDNWSLHH",
+};

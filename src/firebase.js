c19a64fdbf612c3b02e3d9ecb4578c08605a800b
WazzUp05 <49414448+WazzUp05@users.noreply.github.com>
2020-08-21 13:46:47 +0600

    add

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,7 @@
-const firebaseConfig = {
+import firebase from "firebase";
+
+
+const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyDx5A8YiU6fnNze0-Z1UtM1bk_YbQJpoHs",
   authDomain: "instagram-clone-react-9a89b.firebaseapp.com",
   databaseURL: "https://instagram-clone-react-9a89b.firebaseio.com",
@@ -7,4 +10,12 @@ const firebaseConfig = {
   messagingSenderId: "543251687226",
   appId: "1:543251687226:web:256ec5a4ef925b88109203",
   measurementId: "G-MDFDNWSLHH",
-};
+});
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export { db, auth, storage};
+
+
